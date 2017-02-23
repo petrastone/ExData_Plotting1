72bd6729804272ddfee78e906e6b216de970c0dc
Petra Stone <petracstone@gmail.com>
2017-02-23 19:37:03 +0000

    Clean up spacing

--- load_and_clean_data.R
+++ load_and_clean_data.R
@@ -1,7 +1,7 @@
 loadAndCleanData <- function() {
     if(!file.exists("household_power_consumption.txt")){
         url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-        download.file(url, destfile='power_consumption.zip', method="curl")
+        download.file(url, destfile='power_consumption.zip', method="curl" )
         unzip(zipfile = "power_consumption.zip")    
     }
     # Read the file from disk
